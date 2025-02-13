aaea864d424ebd4b0c6b0446e3b6f55d1a8ff8cf
unknown <anita.aleksic@elfak.rs>
2025-02-13 01:07:27 +0100

    Dinastija sve CRUD

--- Back/Controllers/DinastijaController.cs
+++ Back/Controllers/DinastijaController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using KrvNijeVoda.Back.Models;
 using System.Reflection.Metadata;
 using KrvNijeVoda.Back;
+using System.Formats.Tar;
 [Route("api")]
 [ApiController]
 public class DinastijaController : ControllerBase
@@ -19,20 +20,6 @@ public class DinastijaController : ControllerBase
         _client = neo4jService.GetClient();  // Get the Neo4jClient
         _godinaService = godinaService;
     }
-
-
-// [HttpPost("CreateDinastija")]
-// public async Task<IActionResult> CreateDinastija([FromBody] Dinastija dinastija)
-// {
-//     // await _client.Cypher.Create("(d:Dinastija $din)")
-//     //                     .WithParam("din", dinastija)
-//     //                     .ExecuteAsync();
-    
-//     await _client.Cypher.Create("(d:Dinastija $din)")
-//                              .WithParam("din", dinastija)
-//                              .ExecuteWithoutResultsAsync();
-//     return Ok();
-// }
 
     [HttpPost("CreateDinastija")]
     public async Task<IActionResult> CreateDinastija([FromBody] Dinastija dinastija)
@@ -55,89 +42,145 @@ public class DinastijaController : ControllerBase
         return Ok();
     }
 
+    [HttpGet("GetDinastija/{id}")]
+    public async Task<IActionResult> GetDinastija(Guid id)
+    {
+        var din = (await _client.Cypher.Match("(d:Dinastija)")
+                                    .Where((Dinastija d) => d.ID == id)
+                                    .Return(d => d.As<Dinastija>())
+                                    .ResultsAsync)
+                                    .FirstOrDefault();
+        return Ok(din);
+    }
+    [HttpGet("GetAllDinastije")]
+    public async Task<IActionResult> GetAllDinastije()
+    {
+        var dinastije = (await _client.Cypher.Match("(d:Dinastija)")  
+                                             .OptionalMatch("(d)-[:POCETAK_VLADAVINE]->(pg:Godina)") 
+                                             .OptionalMatch("(d)-[:KRAJ_VLADAVINE]->(kg:Godina)")  
+                                             .Return((d, pg, kg) => new {
+                                                 Dinastija = d.As<Dinastija>(),
+                                                 PocetakVladavine = pg.As<Godina>(),
+                                                 KrajVladavine = kg.As<Godina>()
+                                             }) 
+                                             .ResultsAsync)
+                                             .ToList(); 
 
-// [HttpPost("CreateDinastija")]
-// public async Task<IActionResult> CreateDinastija([FromBody] Dinastija dinastija)
-// {
-//     var pocetakGodina = (await _client.Cypher
-//                                       .Match("(g:Godina)")
-//                                       .Where((Godina g) => g.God == dinastija.PocetakVladavine.God) 
-//                                       .Return(g => g.As<Godina>())
-//                                       .ResultsAsync)
-//                                       .FirstOrDefault();
+        if (dinastije == null || !dinastije.Any())
+        {
+            return BadRequest("Nije pronadjena nijedna dinastija!");
+        }
 
-//     pocetakGodina ??= await _godinaService.DodajGodinu(dinastija.PocetakVladavine.God);
-   
-//     var krajGodina = (await _client.Cypher
-//                                   .Match("(g:Godina)")
-//                                   .Where((Godina g) => g.God == dinastija.KrajVladavine.God) 
-//                                   .Return(g => g.As<Godina>())
-//                                   .ResultsAsync)
-//                                   .FirstOrDefault();
+        var result = dinastije.Select(item => new Dinastija {
+                    ID = item.Dinastija.ID,
+                    Naziv = item.Dinastija.Naziv,
+                    Slika = item.Dinastija.Slika,
+                    PocetakVladavine = item.PocetakVladavine ?? new Godina(), //godine ce uvek da nadje ako nisu nullable
+                    KrajVladavine = item.KrajVladavine ?? new Godina() 
+                    //Clanovi = item.Clanovi?.ToList() ?? new List<Licnost>()  // If no Licnost found, return empty list
+                }).ToList();
+
+        return Ok(result);
+    }
 
 
-//     if (krajGodina == null)
-//         krajGodina = await _godinaService.DodajGodinu(dinastija.KrajVladavine.God);
+    [HttpPut("UpdateDinastija/{id}")]
+    public async Task<IActionResult> UpdateDinastija([FromBody] Dinastija dinastija, Guid id)
+    {
+        //ZA LICNOSTI DA BUDE DA SAMO MOZES DA DODAS POSTOJECU LICNOST ILI DA OBRISES NEKU IZ DINASTIJE 
+        //A NE DA MOZES DA UBACUJES KOMPLET NOVU 
+        
+        //zasto mi ne ucita postojece podatke u swagger NE MOZE GOVNO
+        var din = (await _client.Cypher.Match("(d:Dinastija)")
+                                    .Where((Dinastija d) => d.ID == id)
+                                    .OptionalMatch("(d)-[r:POCETAK_VLADAVINE]->(pg:Godina)")
+                                    .OptionalMatch("(d)-[r2:KRAJ_VLADAVINE]->(kg:Godina)")
+                                    .Return((pg, kg) => new 
+                                    {
+                                        Pocetak = pg.As<Godina>(),
+                                        Kraj = kg.As<Godina>()
+                                    })
+                                    .ResultsAsync)
+                                    .FirstOrDefault();
+        
+        if(din == null)
+        {
+            return BadRequest($"Dinastija sa id: {id} ne postoji u bazi!");
+        }
 
-//     await _client.Cypher
-//                  .Match("(pg:Godina {God: $pocetak})", "(kg:Godina {God: $kraj})")
-//                  .Create("(d:Dinastija {ID: $id, Naziv: $naziv, Slika: $slika}) -[:HAS_POCETAK_VLADAVINE]-> (pg), (d) -[:HAS_KRAJ_VLADAVINE]-> (kg)")
-//                  .WithParam("id", Guid.NewGuid())
-//                  .WithParam("naziv", dinastija.Naziv)
-//                  .WithParam("slika", dinastija.Slika)
-//                  .WithParam("pocetak", dinastija.PocetakVladavine.God)
-//                  .WithParam("kraj", dinastija.KrajVladavine.God)
-//                  .ExecuteWithoutResultsAsync();
+        bool promenjenPocetak;
+        bool promenjenKraj = false;
 
-//     return Ok();
-// }
- //Godina pocetakGodinaToUse;
-    // if (pocetakGodina != null)
-    // {
-    //     pocetakGodinaToUse = pocetakGodina;
-    // }
-    // else
-    // {
-    //     pocetakGodinaToUse = await _godinaService.DodajGodinu(dinastija.PocetakVladavine.God);  
-    // }
-    //Godina krajGodinaToUse;
+        if(dinastija.PocetakVladavine != null)
+        {
+            if(din.Pocetak != null)
+            {
+                if(din.Pocetak.God != dinastija.PocetakVladavine.God)
+                {
+                    await _godinaService.DodajGodinu(dinastija.PocetakVladavine.God);
+                    promenjenPocetak = true;
+                }
+                else promenjenPocetak = false;
+            }
+            else 
+            promenjenPocetak = true;
+        }
+        else promenjenPocetak = false;
 
-    // if (krajGodina.Any())
-    // {
-    //     // If the end year 'Godina' exists, use the existing one
-    //     krajGodinaToUse = krajGodina.First();
-    // }
-    // else
-    // {
-    //     // If the end year 'Godina' does not exist, create it
-    //     krajGodinaToUse = await _godinaService.DodajGodinu(dinastija.KrajVladavine.God); 
-    // }
+        if(dinastija.KrajVladavine != null)
+        {
+            if(din.Kraj != null)
+            {
+                if(din.Kraj.God != dinastija.KrajVladavine.God)
+                {
+                    await _godinaService.DodajGodinu(dinastija.KrajVladavine.God);
+                    promenjenKraj = true;
+                }
+            }
+            else
+                promenjenKraj = true;
+        }
 
-[HttpGet("GetDinastija/{id}")]
-public async Task<IActionResult> GetDinastija(Guid id)
-{
-    var din = await _client.Cypher.Match("(d:Dinastija)")
-                                  .Where((Dinastija d) => d.ID == id)
-                                  .Return(d => d.As<Dinastija>())
-                                  .ResultsAsync;
-    return Ok(din.LastOrDefault());
-}
+        //da li nam treba da nam fja vrati novu dinastiju
+        //var novainastija = ().FirstOrDefault();
+        
+        //dinastija postoji => update sve proste atribute
+        await _client.Cypher.Match("(d:Dinastija)")
+                        .Where("d.ID = $id")
+                        .Set("d.Naziv = $naziv, d.Slika = $slika")
+                        .WithParam("id", id)
+                        .WithParam("naziv", dinastija.Naziv)
+                        .WithParam("slika", dinastija.Slika)
+                        .ExecuteWithoutResultsAsync();
+        
+        
+        if(promenjenPocetak)
+        {
+            await _client.Cypher.Match("(d:Dinastija)", "(npg:Godina)") //mora da se mecuje i godina da bi bila referenca na cvor inace je samo na objekat
+                            .Where("d.ID = $id AND npg.God = $pocetak")
+                            .OptionalMatch("(d)-[r:POCETAK_VLADAVINE]->(:Godina)")
+                            .Delete("r")
+                            .Create("(d)-[:POCETAK_VLADAVINE]->(npg)")
+                            .WithParam("id", id)
+                            .WithParam("pocetak", dinastija.PocetakVladavine!.God)
+                            .ExecuteWithoutResultsAsync();
+        }
+        
+        if(promenjenKraj)
+        {
+            await _client.Cypher.Match("(d:Dinastija)", "(nkg:Godina)") //mora da se mecuje i godina da bi bila referenca na cvor inace je samo na objekat
+                            .Where("d.ID = $id AND nkg.God = $kraj")
+                            .OptionalMatch("(d)-[r:KRAJ_VLADAVINE]->(:Godina)")
+                            .Delete("r")
+                            .Create("(d)-[:KRAJ_VLADAVINE]->(nkg)")
+                            .WithParam("id", id)
+                            .WithParam("kraj", dinastija.KrajVladavine!.God)
+                            .ExecuteWithoutResultsAsync();
+        }
 
-[HttpPut("UpdateDinastija/{id}")]
-public async Task<IActionResult> UpdateDinastija([FromBody] Dinastija dinastija, Guid id)
-{
-    //provere za godine ako si oste nista ide ovako
-    //ako nisu mora da  odveze godinu i da veze sa drugom
-    await _godinaService.DodajGodinu(dinastija.PocetakVladavine.God);
-    await _godinaService.DodajGodinu(dinastija.KrajVladavine.God);
 
-    await _client.Cypher.Match("(d:Dinastija)")
-                                  .Where((Dinastija d) => d.ID == id)
-                                  .Set("d = $dinastija")
-                                  .WithParam("dinastija", dinastija)
-                                  .ExecuteWithoutResultsAsync();
-    return Ok();
-}
+        return Ok($"Dinastija {dinastija.Naziv} sa id: {id} je uspesno promenjena!");
+    }
 
     [HttpDelete("DeleteDinastija/{id}")]
     public async Task<IActionResult> DeleteDinastija(Guid id)
--- Back/Models/Dinastija.cs
+++ Back/Models/Dinastija.cs
@@ -4,8 +4,8 @@ namespace KrvNijeVoda.Back.Models{
     public class Dinastija  {
         public Guid ID { get; set; }
         public string Naziv { get; set; }
-        public Godina PocetakVladavine  { get; set; }
-        public Godina KrajVladavine  { get; set; }
+        public Godina PocetakVladavine  { get; set; }//sta ako nisu vladali je l moze to??
+        public Godina KrajVladavine  { get; set; }//AKO SU NULLABLE MORA DA SE MENJA CREATE
         public string? Slika { get; set; }
         public List<Licnost> Clanovi { get; set; } = new List<Licnost>();
         
