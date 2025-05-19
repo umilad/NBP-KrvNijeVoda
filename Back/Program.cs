4c91fc05d2004d8ebedf7cdf27c98773514ad721
unknown <anita.aleksic@elfak.rs>
2025-05-19 20:30:38 +0200

    izmenjen model 2

--- Back/Program.cs
+++ Back/Program.cs
@@ -1,14 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddSingleton<Neo4jService>(new Neo4jService("neo4j+s://84307b33.databases.neo4j.io", "neo4j", "AEYgjhymGHlfJt2zx5MRhtkd6HZaSWCIQGgMITq0u6E"));
-builder.Services.AddScoped<GodinaService>();
-builder.Services.AddScoped<LokacijaService>();
-builder.Services.AddScoped<ZemljaService>();
-builder.Services.AddScoped<RatService>();
-builder.Services.AddScoped<DinastijaService>();
+builder.Services.AddSingleton<Neo4jService>(new Neo4jService("neo4j+s://90ad2e70.databases.neo4j.io", "neo4j", "jGYYLQH3Tdc33sNGdSmpAomQChOhAolLE3mwG-IR4M4"));
+//builder.Services.AddScoped<GodinaService>();
+//builder.Services.AddScoped<LokacijaService>();
+//builder.Services.AddScoped<ZemljaService>();
+//builder.Services.AddScoped<RatService>();
+//builder.Services.AddScoped<DinastijaService>();
 builder.Services.AddControllers();
-builder.Services.AddControllers();
+// builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
