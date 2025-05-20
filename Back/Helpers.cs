65cd50d8d61d6788413002c202e14297e1c855e2
unknown <anita.aleksic@elfak.rs>
2025-05-20 11:01:11 +0200

    godinaStruct

---
+++ Back/Helpers.cs
@@ -0,0 +1,11 @@
+public static struct GodinaStruct {
+    public int God;  
+    public bool PNE;
+
+    // public GodinaStruct(int god, bool pne)
+    // {
+    //     God = god;
+    //     PNE = pne;
+    // }
+
+}
